3c9e4c0e1ef3df7af5248847f28497003c1acd43
Josefine Sandstrom <josefine.sandstrom95@gmail.com>
2018-05-04 09:26:43 +0200

    done with task3

---
+++ homeAssign1/src/G2.java
@@ -0,0 +1,4 @@
+public class G2{
+	public static final int arrivalA = 1, arrivalB = 2, departureA = 3, departureB = 4, MEASURE = 5;
+	public static double time = 0;
+}
--- homeAssign1/src/Template2006.java
+++ homeAssign1/src/Template2006.java
@@ -8,20 +8,18 @@ public class Template2006 {
     	Event actEvent;
     	State actState = new State();
     	new EventList();
-        EventList.InsertEvent(G.arrivalTo1, 1);	
+        EventList.InsertEvent(G.arrivalTo1, 0);	
         EventList.InsertEvent(G.MEASURE, 5);
     	while (actState.noMeasurements < 10000){
     		actEvent = EventList.FetchEvent();
     		G.time = actEvent.eventTime;
     		actState.TreatEvent(actEvent);
     	}
-    	double meanQ1 = 1.0*actState.accumulated1/actState.noMeasurements;
-    	double rejectedProb = 1.0*actState.noRejected/(meanQ1+actState.noRejected);
-    	
-    	System.out.println(1.0*actState.accumulated2/actState.noMeasurements);	//The mean nr of customers in queue 2
+    	double rejectedProb = 1.0*actState.noRejected/actState.noArrivalTo1;
+    	System.out.println("Mean number of customers in Q2: " + 1.0*actState.accumulated2/actState.noMeasurements);	//The mean nr of customers in queue 2
     	//System.out.println(actState.accumulated);	//Nr of customers in queue when sampling
     	System.out.println(actState.noMeasurements);	//Number of measurements 
-    	System.out.println(rejectedProb);	//the probability that a customer is rejected at Q1
+    	System.out.println("Prob rejection: " + rejectedProb);	//the probability that a customer is rejected at Q1
     	
     	actState.W.close();
     }
