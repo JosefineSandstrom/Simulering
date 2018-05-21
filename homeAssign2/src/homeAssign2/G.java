f504bf2bb24f2f0318fa42281a768d3ec933b861
Josefine Sandstrom <josefine.sandstrom95@gmail.com>
2018-05-21 18:47:16 +0200

    wrote stuff on configFile

---
+++ homeAssign2/src/homeAssign2/G.java
@@ -0,0 +1,6 @@
+package homeAssign2;
+
+public class G {
+	public static final int WAKEUP = 1, SEND = 2, ARRIVAL = 3, PROCESSING = 4;
+	public static double time = 0;
+}
---
+++ homeAssign2/src/homeAssign2/mainSimulation.java
@@ -0,0 +1,23 @@
+package homeAssign2;
+import java.util.*;
+
+
+public class mainSimulation {
+	
+
+	public static void main(String[] args) {
+		
+		Signal actSignal;
+		new SignalList();
+		
+		SimpleFileWriter W = new SimpleFileWriter("configFile", false);
+		
+		
+		
+		
+		
+		W.close();
+
+	}
+
+}
