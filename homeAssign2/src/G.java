4942e2182312f8065a400f5f33098fd6c4a695c8
unknown <joel.bill94@gmail.com>
2018-05-22 09:30:47 +0200

    Without package

---
+++ homeAssign2/src/G.java
@@ -0,0 +1,5 @@
+
+public class G {
+	public static final int WAKEUP = 1, SEND = 2, ARRIVAL = 3, PROCESSING = 4;
+	public static double time = 0;
+}
--- homeAssign2/src/homeAssign2/G.java
+++
@@ -1,6 +0,0 @@
-package homeAssign2;
-
-public class G {
-	public static final int WAKEUP = 1, SEND = 2, ARRIVAL = 3, PROCESSING = 4;
-	public static double time = 0;
-}
